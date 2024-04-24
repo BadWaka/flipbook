7fabd323eab6fe109f4af0d81448fd7bd7813fc0
BadWaka <1456683844@qq.com>
2024-04-24 14:40:35 +0800

    feat: demo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div
+        class="wrapper"
+    >
+        <FlipBook
+            :selected-index="0"
+        >
+            <div
+                v-for="item in list"
+                :style="{
+                    backgroundImage: `url(${item.src})`
+                }"
+                class="item flip-book-item"
+            >
+            </div>
+        </FlipBook>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import {
+    ref
+} from 'vue';
+import FlipBook from './components/FlipBook/FlipBook.vue';
+
+const list = ref([
+    {
+        src: 'https://5b0988e595225.cdn.sohucs.com/images/20200105/d65d774079e242f19429a8bb730394f1.jpeg'
+    },
+    {
+        src: 'https://img0.baidu.com/it/u=3549641921,2392395869&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=890'
+    },
+    {
+        src: 'https://img2.baidu.com/it/u=607246426,4214908815&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=1281'
+    }
+]);
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+    height: 100vh;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.item {
+    position: absolute;
+    top: -1px;
+    left: -1px;
+    right: -1px;
+    bottom: -1px;
+    background-size: cover;
+    background-repeat: no-repeat;
 }
 </style>
